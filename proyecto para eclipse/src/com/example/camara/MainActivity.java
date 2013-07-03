f27ca87fbed94a9c3f1ca5036c5397b6ec8d89de
gerardo <barratkudar@gmail.com>
2013-07-03 12:43:11 -0500

    subo nueva version del proyecto
    subo nueva version del proyecto camara con la funcionalidad de obtener
    camara y hacer mostrar un preview de esa misma foto

---
+++ proyecto para eclipse/src/com/example/camara/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.camara;
+
+import android.os.Bundle;
+import org.apache.cordova.*;
+public class MainActivity extends DroidGap
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        super.loadUrl(Config.getStartUrl());
+        //super.loadUrl("file:///android_asset/www/index.html")
+    }
+}
